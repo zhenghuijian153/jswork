563acade89ddb630a3044f1aa3b04e56d023f66d
zhenghuijian153 <1751075074@qq.com>
2019-10-14 18:37:50 +0800

    work5

---
+++ work5/work5.js
@@ -0,0 +1,35 @@
+
+//9x9的空表，请自行编写这段代码
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+  str +='<tr>'
+  for(var j=9;j>0;--j){
+    str +='<td>&nbsp;</td>'
+  }
+  str +='</td>'
+}
+str +='</table>'
+document.getElementById('table1').innerHTML = str
+//倒九九乘法空表
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+  str +='<tr>'
+  for(var j=i;j>0;--j){
+    str +='<td>&nbsp;</td>'
+  }
+  str +='</td>'
+}
+str +='</table>'
+document.getElementById('table2').innerHTML = str
+//倒九九乘法表，请自行编写这段代码
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+  str +='<tr>'
+  for(var j=i;j>0;--j){
+    str +='<td>' + j + '*' + i + '=' + ( j * i )+'</td>'
+  }
+  str +='</td>'
+}
+str +='</table>'
+document.getElementById('table3').innerHTML = str
+
