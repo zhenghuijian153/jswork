acc3b4303c21d48a83e4afe0f8bf10108e5c2550
zhenghuijian153 <1751075074@qq.com>
2019-10-21 16:38:51 +0800

    work7

---
+++ work7/index.js
@@ -0,0 +1,14 @@
+function sort(method) {
+    let origin = document.getElementById('origin').value
+    let logs
+
+    if(method ==='bubble'){
+        logs = bubbleSort(origin)
+    }
+    else if(method ==='insert'){
+        logs = insertSort(origin)
+    }
+     showDiv = document.getElementById('Anim')
+    logValues = logs.values()
+        setTimeout("showLog(logValues,showDiv)",50)
+}
