ef733af7b59bff6ede52de25236407ab5f776e14
zhenghuijian153 <1751075074@qq.com>
2019-12-23 20:04:33 +0800

    work18

--- work18/index1.js
+++ work18/index1.js
@@ -111,14 +111,14 @@
         last: document.getElementById('page_last'),
         pageNum: document.getElementById('page_num'),
         onChange: function (){
-             comment.ajax('http://localhost:8080/ajax?page=' + this.page, function () {  
+             /*comment.ajax('http://localhost:8080/ajax?page=' + this.page, function () {  
                  progressBar = new ProgressBar(progressContainer);
                  progressBar.show();
+            },*/
+            comment.ajax('http://139.9.81.203:8090/ajax?page=' + this.page, function () {  
+                progressBar = new ProgressBar(progressContainer);
+                progressBar.show();
             },
-            //comment.ajax('http://139.9.81.203:8090/ajax?page=' + this.page, function () {  
-            //    progressBar = new ProgressBar(progressContainer);
-            //    progressBar.show();
-            //},
             function (obj) {
                 pageList.maxPage = obj.maxPage;
                 pageList.updateStatus();
